183416444aca2fcb310a46d02ad2a81d72fddc23
itsamanverma <amanvermame786@gmail.com>
2019-04-01 15:32:14 +0530

    notes controller & notes model

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
+use Facades\App\Notes;
 
 class NotesController extends Controller
 {
--- app/Notes.php
+++ app/Notes.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model; 
 
 class Notes extends Model
 {
-    //
+    
 }
