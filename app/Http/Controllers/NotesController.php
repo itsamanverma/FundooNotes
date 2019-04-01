0f5e4822addec29cf71f87927bc84d044beec723
itsamanverma <amanvermame786@gmail.com>
2019-04-01 19:38:01 +0530

    notes controller & notes model

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -6,9 +6,33 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
-use Facades\App\Notes;
+use \App\Notes;
 
 class NotesController extends Controller
 {
-    //
+    public function __construct(){
+
+    }
+
+    /**
+     * write the function to create the new notes
+     * 
+     * @return response
+     */
+    public function create(Request $req){
+       $data = $req->all();
+       $data['userid'] = Auth::user()->id;
+       $note = Notes::createNewNote($data);
+       return response()->json(['message' => 'Note Created','id'=> $note->id],201);
+    }
+    /**
+     * write the function to get all the notes of the user
+     * 
+     * @return response 
+     */
+     public function getNotes()
+     {
+
+     }  
+
 }
--- app/Notes.php
+++ app/Notes.php
@@ -2,7 +2,10 @@
 
 namespace App;
 
+use Cache;
 use Illuminate\Database\Eloquent\Model; 
+use Illuminate\Support\Facades\Auth;
+use Laravel\Passport\Bridge\User;
 
 class Notes extends Model
 {
