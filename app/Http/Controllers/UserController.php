908ac4f2a32f9848aac6fd65f659cef64ad94696
itsamanverma <amanvermame786@gmail.com>
2019-04-16 19:47:43 +0530

    changes in UserContrtoller & starting to do addProfile api in laravel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -162,14 +162,34 @@ class UserController extends Controller
             return response()->json(['token' => $token ,'userdetails' => Auth::user()],200);   
         }
 
-         /**
+        /**
           * function to add the profile pic of the user
           * 
           * @var Request
           * @return Response
           */
-          public function addProfilepic(Request $re){
-              
-          }
+           public function addProfilepic(Request $req){
+           $reqss = $req->all();
+           if($req->hasFile('profilepic')){
+            //filename
+            $origImage=$req->file('profilepic');
+            $ext = $req->file('profilepic')->getClientOriginalExtension();
+            //if image is svg
+            if($ext==='svg'){
+                $ext='svg+xml';
+            }
+            //getting the path of image in temp folder
+            $path = $req->file('profilepic')->getRealPath();
+            //converting to base64 to save it in database
+            $base64 = 'data:image/' . $ext . ';base64,' . base64_encode(file_get_contents($path));
+            //getting the authenticated user
+            $user= Auth::user();
+            //adding the profile pic to the user and saving it in the database
+            $user->profilepic = $base64;
+            $user->save();
+            //returning the response to the user
+            return response()->json(['message'=>'done','data'=> User::with('labels')->find($user->id)],200);
+        }
+     }
 }
  
