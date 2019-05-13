01aef65bea58e4eff16da8117e200e87b1af5993
itsamanverma <amanvermame786@gmail.com>
2019-05-13 19:36:07 +0530

    try to make swagger json

---
+++ development/swagger-v1.php
@@ -0,0 +1,31 @@
+`<?php
+/**
+ * @SWG\Swagger(
+ *     schemes={"http"},
+ *     host=API_HOST,
+ *     basePath="/",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="Swagger Integration with PHP Laravel",
+ *         description="Integrate Swagger in Laravel application",
+ *         termsOfService="",
+ *         @SWG\Contact(
+ *             email="sachit.wadhawan@quovantis.com"
+ *         ),
+ *     ),
+ * )
+ *
+ @SWG\Swagger(
+      schemes={"http"},
+      host=API_HOST,
+      basePath="/",
+      @SWG\Info(
+         version="1.0.0",
+          title="Laravel and Swagger",
+          description="Getting started with Laravel and Swagger",
+          termsOfService="",
+          @SWG\Contact(
+              email="name@example.com"
+          ),
+      ),
+  )
